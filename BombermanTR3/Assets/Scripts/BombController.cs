d133a1704143962d8896086172c9f031b553039b
Fiona <a24fiomongia@inspedralbes.cat>
2026-04-11 18:32:15 +0200

    animación personaje bombas y explosion

---
+++ BombermanTR3/Assets/Scripts/BombController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic;
+
+
+public class BombController : MonoBehaviour
+{
+
+    [Header("Bomba")]
+    public GameObject bombPrefab;
+    public KeyCode inputKey = KeyCode.Space;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+    private int bombsRemaining;
+
+[Header("Explosion")]
+    public Explosion explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        if (bombsRemaining> 0 && Input.GetKeyDown(inputKey)){
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb = Instantiate(bombPrefab,position, Quaternion.identity);
+        bombsRemaining --;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab,position,Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        Destroy(explosion.gameObject,explosionDuration);
+        
+        Destroy(bomb);
+        bombsRemaining++;
+        }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Bomb")){
+            other.isTrigger = false; 
+        }
+    }
+}
--- BombermanTR3/Assets/Scripts/MovementController.cs
+++ BombermanTR3/Assets/Scripts/MovementController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic;
 
-public class MovementController : MonoBehaviour
+public class MovementController: MonoBehaviour  
 {
-    public new Rigidbody2D rigidbody { get;private set;}
+    public new Rigidbody2D rigidbody {get; private set;}
     private Vector2 direction = Vector2.down;
     public float speed = 5f;
 
@@ -11,25 +13,30 @@ public class MovementController : MonoBehaviour
     public KeyCode inputLeft = KeyCode.A;
     public KeyCode inputRight = KeyCode.D;
 
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
     private void Awake()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
-        
+        rigidbody = GetComponent <Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
     }
 
     private void Update()
     {
         if (Input.GetKey(inputUp)) {
-            SetDirection(Vector2.up);
+            SetDirection(Vector2.up, spriteRendererUp);
         } else if (Input.GetKey(inputDown)) {
-            SetDirection(Vector2.down);
+            SetDirection(Vector2.down, spriteRendererDown);
         } else if (Input.GetKey(inputLeft)) {
-            SetDirection(Vector2.left);
+            SetDirection(Vector2.left, spriteRendererLeft);
         } else if (Input.GetKey(inputRight)) {
-            SetDirection(Vector2.right);
-        }else{
-            SetDirection(Vector2.zero);
-            
+            SetDirection(Vector2.right, spriteRendererRight);
+        } else {
+            SetDirection(Vector2.zero, activeSpriteRenderer);
         }
     }
 
@@ -40,10 +47,17 @@ public class MovementController : MonoBehaviour
 
         rigidbody.MovePosition(position + translation);
     }
-    private void SetDirection (Vector2 newDirection)
+
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
     {
-        direction= newDirection;
+        direction = newDirection;
+        
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp; 
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
     }
-
-
 }
